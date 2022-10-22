4a873f608b24385795fbc050ddd509e5e78b21a1
Gabriel Matos <gsilva505@hotmail.com>
2022-10-21 21:52:42 -0300

    criando o card de pontos e o timer do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,81 @@ import react, { useEffect, useState } from 'react'
 import './App.css'
 import Sidebar from './components/Sidebar'
 
+const getRandomColor = () => {
+  const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',]
+  const color = new Array(6).fill('').map(() => digits[Math.floor(Math.random() * digits.length)]).join("")
+  return `#${color}`
+}
+
 function App() {
 
   const [color, setColor] = useState("")
   const [answers, setAnswers] = useState([])
+  const [isWrong, setIsWrong] = useState(false)
 
-  const getRandomColor = () => {
-    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',]
-    const color = new Array(6).fill('').map(() => digits[Math.floor(Math.random() * digits.length)]).join("")
-    return `#${color}`
-  }
+  const [progress, setProgress] = useState(30);
+  const [start, setStart] = useState(false)
 
 
   useEffect(() => {
+    setTimeout(() => {
+      if (progress === 0) {
+        setProgress(0)
+        console.log('terminou');
+        console.log(progress);
+      } else {
+        setProgress(progress - 1)
+        console.log(progress);
+      }
+    }, 1000)
+  }, [progress])
+
+  const handleAnswerClicked = (e, answer) => {
+    if (answer === color) {
+      setIsWrong(true)
+      console.log("Voce acertou: ", e.target.textContent);
+      generateColor()
+    } else {
+      setIsWrong(false)
+      generateColor()
+      console.log("Voce errou: ", e.target.textContent, "a certa era: ", color);
+    }
+  }
+
+  const generateColor = () => {
     const actualColor = getRandomColor()
     setColor(actualColor);
-    console.log(actualColor);
     setAnswers([actualColor, getRandomColor(), getRandomColor()].sort(() => 0.5 - Math.random()))
+  }
+
+  useEffect(() => {
+    generateColor()
   }, [])
 
   return (
     <div className="App">
       <Sidebar />
       <div className='color__game'>
-        <label htmlFor="">Your Name: </label>
-        <input type="text" style={{ padding: '5px', width: '300px', height: '30px', marginBottom: '5px', borderRadius: '10px', fontSize: '1.2rem' }} />
+        {/* <label htmlFor="">Your Name: </label>
+        <input type="text" style={{ padding: '5px', width: '300px', height: '30px', marginBottom: '5px', borderRadius: '10px', fontSize: '1.2rem' }} /> */}
         <hr />
         <h2>Guess the color</h2>
-        <div className='color_div' style={{ background: color }}></div>
+        <progress value={progress} max="30" min="0" style={{ width: '400px', height: '10px', borderRadius: '0px', color: '#fff' }} />
+        <div className='color_div' style={{ background: color, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <button style={{ width: '150px', height: '40px', color: 'white', background: '#292929', border: 'none', cursor: 'pointer' }}>Start</button>
+        </div>
         <div>
           {
             answers.map(answer => (
-              <button style={{ marginRight: "5px" }} key={answer}>{answer}</button>
+              <button
+                onClick={(e) => handleAnswerClicked(e, answer)}
+                className='color__selected'
+                key={answer}>{answer}</button>
             ))
           }
         </div>
       </div>
+      <a style={{ position: 'fixed', bottom: '40px', right: '40px', cursor: 'pointer' }}>Reset all data</a>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 import './sidebar.css'
+import wrong from '../assets/wrong.png'
+import correct from '../assets/check.png'
+
+
+const SideCardWrong = () => {
+    return (
+        <div style={{ marginTop: '30px', marginLeft: '50px', display: 'flex' }}>
+            <div className='sideColorWrong' style={{ background: '#326754' }}>#326754</div>
+            <div className='sideColorWrong' style={{ background: '#825754' }}>#825754</div>
+            <p><img src={wrong} style={{ width: '20px' }} alt="Wrong Guess" /> 2 Pts</p>
+        </div>
+
+    )
+}
+const SideCardCorrect = () => {
+    return (
+        <div style={{ marginTop: '30px', marginLeft: '50px', display: 'flex' }}>
+            <div className='sideColorCorrect' style={{ background: '#990101' }}>#990101</div>
+            <p><img src={correct} style={{ width: '20px' }} alt="correct Guess" /> 5 Pts</p>
+        </div>
+
+    )
+}
 
 export default function Sidebar() {
     return (
@@ -7,7 +30,8 @@ export default function Sidebar() {
             <h2>Current/Latest game</h2>
             <hr />
             <span >Guessed Color | Correct Color | Score</span>
-            <div style={{ marginTop: '30px' }}>Card</div>
+            <SideCardWrong />
+            <SideCardCorrect />
         </div>
     )
 }
