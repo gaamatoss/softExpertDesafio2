7a3a815e364190ed69a672f9e84f5bb308642767
Gabriel Matos <gsilva505@hotmail.com>
2022-10-27 23:02:14 -0300

    passando cores para o component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -10,44 +10,61 @@ const getRandomColor = () => {
 
 function App() {
 
-  const [color, setColor] = useState("")
-  const [answers, setAnswers] = useState([])
+  const [color, setColor] = useState('')
+  const [generatedAnswers, setGeneratedAnswers] = useState([])
   const [clickedAnswer, setClickedAnswer] = useState([])
+  const [correctAnswer, setCorrectAnswer] = useState([])
   const [isWrong, setIsWrong] = useState(false)
   const [start, setStart] = useState(false)
+  const [answers, setAnswers] = useState([])
 
+
+  const valores = {
+    clickedAnswer,
+    correctAnswer
+  }
+
+  const testeAnswer = () => {
+    setAnswers(valores)
+    console.log("respostas", answers);
+  }
 
 
   const handleAnswerClicked = (e, answer) => {
+    const clickedValue = e.target.textContent
     if (answer === color) {
       setIsWrong(true)
-      console.log("Voce acertou + 5 Pts");
-      setClickedAnswer(e.target.textContent)
       generateColor()
+      setClickedAnswer(clickedValue)
+      setCorrectAnswer(color)
     } else {
       setIsWrong(false)
       generateColor()
-      console.log("Voce errou - 1 Pt");
-      setClickedAnswer(e.target.textContent)
+      setClickedAnswer(clickedValue)
+      setCorrectAnswer(color)
     }
   }
 
   const generateColor = () => {
     const actualColor = getRandomColor()
     setColor(actualColor);
-    setAnswers([actualColor, getRandomColor(), getRandomColor()].sort(() => 0.5 - Math.random()))
+    setGeneratedAnswers([actualColor, getRandomColor(), getRandomColor()].sort(() => 0.5 - Math.random()))
   }
 
   useEffect(() => {
     generateColor()
-  }, [])
+  }, [start])
+
+  // useEffect(() => {
+  //   console.log(generatedAnswers, clickedAnswer, correctAnswer);
+
+  // }, [])
+
 
   return (
     <div className="App">
-      <Sidebar answers={clickedAnswer} color={color} />
+      <Sidebar answers={clickedAnswer} color={correctAnswer} />
       <div className='color__game'>
-        {/* <label htmlFor="">Your Name: </label>
-        <input type="text" style={{ padding: '5px', width: '300px', height: '30px', marginBottom: '5px', borderRadius: '10px', fontSize: '1.2rem' }} /> */}
         <hr />
         <h2>Guess the color</h2>
         {/* <progress value={progress} max="30" min="0" style={{ width: '400px', height: '10px', borderRadius: '0px', color: '#fff' }} /> */}
@@ -58,7 +75,7 @@ function App() {
         </div>
         <div>
           {
-            start ? answers.map(answer => (
+            start ? generatedAnswers.map(answer => (
               <button
                 onClick={(e) => handleAnswerClicked(e, answer)}
                 className='color__selected'
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,20 +4,20 @@ import wrong from '../assets/wrong.png'
 import correct from '../assets/check.png'
 
 
-const SideCardWrong = () => {
+const SideCardWrong = (props) => {
     return (
         <div style={{ marginTop: '30px', marginLeft: '50px', display: 'flex' }}>
-            <div className='sideColorWrong' style={{ background: '#326754' }}>#326754</div>
-            <div className='sideColorWrong' style={{ background: '#825754' }}>#825754</div>
+            <div className='sideColorWrong' style={{ background: props.answers }}>{props.answers}</div>
+            <div className='sideColorWrong' style={{ background: props.color }}>{props.color}</div>
             <p><img src={wrong} style={{ width: '20px' }} alt="Wrong Guess" /> 2 Pts</p>
         </div>
 
     )
 }
-const SideCardCorrect = () => {
+const SideCardCorrect = (props) => {
     return (
         <div style={{ marginTop: '30px', marginLeft: '50px', display: 'flex' }}>
-            <div className='sideColorCorrect' style={{ background: '#990101' }}>#990101</div>
+            <div className='sideColorCorrect' style={{ background: props.color }}>{props.color}</div>
             <p><img src={correct} style={{ width: '20px' }} alt="correct Guess" /> 5 Pts</p>
         </div>
 
@@ -31,9 +31,9 @@ export default function Sidebar(props) {
             <hr style={{ width: '300px' }} />
             <span >Guessed Color | Correct Color | Score</span>
             <div className='sideAnswers'>
-                {/* <SideCardWrong />
-                <SideCardCorrect /> */}
-                <p>{props.answers} : {props.color}</p>
+                {
+                    props.answers === props.color ? <SideCardCorrect color={props.color} /> : <SideCardWrong answers={props.answers} color={props.color} />
+                }
             </div>
         </div>
     )
